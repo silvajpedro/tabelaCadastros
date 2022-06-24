f856a8ae731a5167d6061e518bca3b1b3ad8fb48
silvajpedro <belojpedro.31@gmail.com>
2022-06-24 12:55:22 -0300

    ifs adicionados caso algumas condições aconteçam

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
-
 let nomes = []
 let idade = []
 let telefone = []
 let email = []
 alert("Bem Vindo ao Sistema de Cadastros CRD")
 let yesOrNo = confirm("Deseja fazer o cadastro de alguém?")
-let quantity = Number(prompt("Deseja cadastrar quantas pessoas?"))
-if(yesOrNo == true){
-    for(let i = 1; i <= quantity ; i++){
+
+if (yesOrNo == true) {
+    let quantity = Number(prompt("Deseja cadastrar quantas pessoas?"))
+    for (let i = 1; i <= quantity; i++) {
         nomes[i] = prompt(`Digite o ${i}° nome a ser cadastrado`)
-        idade[i] = prompt(`Digite a idade referente ao ${i}° nome cadastrado `)
-        telefone[i] = prompt(`Digite o telefone referente a ${i}° pessoa cadastrada`)
-        email[i] = prompt()
+        idade[i] = Number(prompt(`Digite a idade referente a ${i}° pessoa `))
+        telefone[i] = Number(prompt(`Digite o telefone referente a ${i}° pessoa `))
+        email[i] = prompt(`Digite o email referente a ${i}° pessoa `)
     }
-    
+    let lastConfirm = confirm("Deseja visualizar as pessoas cadastradas??")
+
+    if (lastConfirm == true) {
+        while (lastConfirm == true) {
+            let position = Number(prompt("Qual posição cadastrada deseja visualizar"))
+            alert(`A ${position}° posição tem como cadastro: \n Nome:${nomes[position]} \n Idade:${idade[position]} \n Telefone:${telefone[position]} \n Email:${email[position]}`)
+            lastConfirm = confirm("Deseja visualizar outro cadastro?")
+            if (lastConfirm == false) {
+                alert("Obrigado por utilizar nosso sistema ;D")
+            }
+        }
+    } else {
+        alert("Você cadastrou as pessoas porém decidiu não visualiza-lás")
+    }
+} else {
+    alert("Você não cadastrou ninguém ;( ")
 }
 
-let lastConfirm = confirm("Deseja visualizar as pessoas cadastradas??")
-
-if(lastConfirm == true){
-    let position = Number(prompt("Qual posição cadastrada deseja visualizar"))
-    alert(`A ${position}° posição tem como cadastro: \n Nome:${nomes[position]} \n Idade:${position} \n Telefone:${telefone[position]} \n Email:${email}`)
-}
-//necessita de alguns ajustes email e ajeitar esse if para fazer o programa finalizar certinho
-// linha 14 email e linha 21 if
+console.log(nomes)
+console.log(idade)
+console.log(telefone)
+console.log(email)
+// consoles apenas para fins de consulta
+// codigo ainda necessita de revisao feito apenas para fins práticos
